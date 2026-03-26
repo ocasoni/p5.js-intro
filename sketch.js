6505627d1e375adf994ac53a5921b841a796b7b6
nik <n.somma@ied.edu>
2026-03-26 16:14:59 +0100

    immages collages

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
-function setup() {
-  createCanvas(400, 400);
+let topolino;
+
+function preload() {
+  topolino = loadImage("image.png");
 }
 
-function draw() {
-  background(220);
+
+function setup() {
+  createCanvas(400, 400);
+  background(160, 0, 0);
+
+  let x;
+  let y;
+
+  for (let i = 0; i < 100; i = i + 1) {
+    x = random(300);
+    y = random(300);
+    image(topolino, x, y, 100, 50);
+  }
 }
+
+
